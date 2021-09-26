3898200be6f5d8e5adb04e3741fcb9db2eac285d
khamaldeeen <Aiyelesok@gmail.com>
2021-09-26 20:48:45 +0100

    First Commit. Deployed to firebase

---
+++ public/main.js
@@ -0,0 +1,30 @@
+var menuItems = document.getElementById("menuItems");
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle() {
+    if (menuItems.style.maxHeight == "0px") {
+        menuItems.style.maxHeight = "200px";
+    } else {
+        menuItems.style.maxHeight = "0px";
+    }
+}
+
+
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
