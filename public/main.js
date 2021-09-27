37f05b75565c62806bdafce639bf694517bb0f67
khamaldeeen <Aiyelesok@gmail.com>
2021-09-27 14:10:33 +0100

    Added modal for return policy

--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
 const menuItems = document.getElementById('menuItems');
 const mybutton = document.getElementById('myBtn');
+// Modal variables
+const modal = document.getElementById('myModal');
+const closemodal = document.getElementsByClassName('close')[0];
+const btnModal = document.getElementById('openModal');
+
+//User clicks on "Return policy" and invokes the modal page
+btnModal.onclick = () => {
+  modal.style.display = 'block';
+};
+
+// User action on clicking on the close button on modal
+closemodal.onclick = () => {
+  modal.style.display = 'none';
+};
+
+//User action on clicking anywhere outside of the modal page 
+window.onclick = function(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
 menuItems.style.maxHeight = '0px';
 
