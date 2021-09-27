5ea0cc1e42dc739b08d4ae5f8ca7fbc3023092dc
Badmus Adeshina <badmusadeshinna@gmail.com>
2021-09-27 11:05:34 +0100

    sortedissue

--- public/main.js
+++ public/main.js
@@ -1,30 +1,33 @@
-var menuItems = document.getElementById("menuItems");
+const menuItems = document.getElementById('menuItems');
+const mybutton = document.getElementById('myBtn');
 
-menuItems.style.maxHeight = "0px";
+menuItems.style.maxHeight = '0px';
 
-function menuToggle() {
-    if (menuItems.style.maxHeight == "0px") {
-        menuItems.style.maxHeight = "200px";
-    } else {
-        menuItems.style.maxHeight = "0px";
-    }
-}
+window.onload = function () {
+  scrollFunction();
+};
 
+const menuToggle = () => {
+  if (menuItems.style.maxHeight == '0px') {
+    menuItems.style.maxHeight = '200px';
+  } else {
+    menuItems.style.maxHeight = '0px';
+  }
+};
 
+window.onscroll = () => {
+  scrollFunction();
+};
 
-var mybutton = document.getElementById("myBtn");
+const scrollFunction = () => {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+};
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
